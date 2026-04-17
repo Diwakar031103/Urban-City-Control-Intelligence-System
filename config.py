618c5ff56632fe18e506898b755a78de7ea3cc06
Diwakar Pandey <diwakarpandey0311@gmail.com>
2026-04-17 20:34:11 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+# mapping of internal zone IDs to human-readable names
+# used for display in UI / logs instead of raw IDs
+ZONES = {
+    "thane_zone_1": "Thane Zone 1",
+    "thane_zone_2": "Thane Zone 2",
+    "mumbai_zone_1": "Mumbai Zone 1",
+    "mumbai_zone_2": "Mumbai Zone 2",
+    "navi_mumbai_zone_1": "Navi Mumbai Zone 1",
+    "navi_mumbai_zone_2": "Navi Mumbai Zone 2"
+}
